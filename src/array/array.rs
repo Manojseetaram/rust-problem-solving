5ebbbd1bb7ea4c16e5bd27b488792f5dcc56ba53
Manojseetaram <manojseetaram.artytech@gmail.com>
2025-08-30 18:36:51 +0530

    smalest number

--- src/array/array.rs
+++ src/array/array.rs
@@ -13,13 +13,24 @@ pub fn largest_number(s : Vec<i32>)-> i32{
     }
     num
 } 
+//3. Find the smalest number 
+pub fn smalest_number(s : Vec<i32>)-> i32{
+  let mut num = s[0];
+  for i in s.iter(){
+    if *i < num {
+        num = *i
+    }
+  }
+  num
+}
 pub fn array_operation(){
      let num = vec!['1' , '2' ,'3' , '4' , '5','6'];
      let num_op = vec![1 ,2 ,3 ,4 , 5 ,6];
 
 
      println!("This is revrse number : {:#?}" ,revrse_num(num));
-     println!("The largest number is : {}", largest_number(num_op));
-     
+     println!("The largest number is : {}", largest_number(num_op.clone()));
+     println!("The smalest number is : {}", smalest_number(num_op.clone()));
+
 
 }
